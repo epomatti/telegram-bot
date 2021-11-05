150f221d1fe551faec36985b1b0cd57800b00202
epomatti <evandro.pomatti@gmail.com>
2021-11-05 13:10:04 -0300

    offset

--- src/sandbox.js
+++ src/sandbox.js
@@ -3,17 +3,17 @@ const axios = require('axios');
 
 const bot = process.env.BOT
 const token = process.env.TOKEN
+const offset = process.env.OFFSET
 
 // Test the API
 const getMe_url = `${bot}${token}/getMe`
 
 axios.get(getMe_url)
     .then(function (response) {
-        console.log(`Get Me: ${response.status}`)
+        //console.log(`Get Me: ${response.status}`)
     })
 
 // Get Updates
-const offset = 750513351
 const getUpdates_url = `${bot}${token}/getUpdates?offset=${offset}`
 
 axios.get(getUpdates_url)
